02065006c28ad6dd50de14ee51f7bd141ac9ff1c
Gyuhyeon <weanother@gmail.com>
2018-03-19 23:23:21 +0900

    Finished As2

--- CS250/AS2/CubeMesh.cpp
+++ CS250/AS2/CubeMesh.cpp
@@ -9,23 +9,45 @@ Term : Spring 2018
 const Point CubeMesh::vertices[8] = 
 {
 	Point(1.0f, 1.0f, 1.0f),
-	Point(-1.0f, 1.0f, 1.0f),
-	Point(-1.0f, -1.0f, 1.0f),
-	Point(1.0f, -1.0f, 1.0f),
 	Point(1.0f, 1.0f, -1.0f),
-	Point(-1.0f, 1.0f, -1.0f),
-	Point(-1.0f, -1.0f, -1.0f),
+	Point(1.0f, -1.0f, 1.0f),
 	Point(1.0f, -1.0f, -1.0f),
+	Point(-1.0f, 1.0f, 1.0f),
+	Point(-1.0f, 1.0f, -1.0f),
+	Point(-1.0f, -1.0f, 1.0f),
+	Point(-1.0f, -1.0f, -1.0f),
+};
+
+const CubeMesh::Face CubeMesh::faces[12] =
+{
+	CubeMesh::Face(0, 4, 6),
+	CubeMesh::Face(0, 6, 2),
+	CubeMesh::Face(1, 7, 5),
+	CubeMesh::Face(1, 3, 7),
+	CubeMesh::Face(4, 5, 7),
+	CubeMesh::Face(4, 7, 6),
+	CubeMesh::Face(0, 3, 1),
+	CubeMesh::Face(0, 2, 3),
+	CubeMesh::Face(1, 5, 4),
+	CubeMesh::Face(1, 4, 0),
+	CubeMesh::Face(2, 6, 3),
+	CubeMesh::Face(6, 7, 3),
 };
 
 const CubeMesh::Edge CubeMesh::edges[12] = 
 {
-	CubeMesh::Edge();
-};
-
-const CubeMesh::Face CubeMesh::faces[12] = 
-{
-
+	CubeMesh::Edge(0, 1),
+	CubeMesh::Edge(1, 5),
+	CubeMesh::Edge(5, 4),
+	CubeMesh::Edge(4, 0),
+	CubeMesh::Edge(0, 2),
+	CubeMesh::Edge(1, 3),
+	CubeMesh::Edge(5, 7),
+	CubeMesh::Edge(4, 6),
+	CubeMesh::Edge(2, 3),
+	CubeMesh::Edge(3, 7),
+	CubeMesh::Edge(7, 6),
+	CubeMesh::Edge(6, 2),
 };
 
 int CubeMesh::VertexCount(void)
@@ -42,12 +64,12 @@ Point CubeMesh::GetVertex(int i)
 
 Vector CubeMesh::Dimensions(void)
 {
-	return Vector(1.0f, 1.0f, 1.0f);
+	return Vector(0.0f, 0.0f, 0.0f);
 }
 
 Point CubeMesh::Center(void)
 {
-	return Point(1.0f, 1.0f, 1.0f);
+	return Point(0.0f, 0.0f, 0.0f);
 }
 
 int CubeMesh::FaceCount(void)
